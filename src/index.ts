32e60a33c3fc022e9f365bb9044e351e0335da75
Ryan Page <ryanapage@gmail.com>
2021-06-02 19:41:11 -0700

    minor things

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
 const dotenv = require('dotenv');
-dotenv.config({
-    path: '.env'
-});
-dotenv.config({
-    path: '.env.public'
-})
-import { CronJob } from 'cron';
+dotenv.config({ path: '.env' });
+dotenv.config({ path: '.env.public' })
 import Fastify from 'fastify';
-import path from 'path';
 import { startCronJob } from './util/cronjob';
-import { discoverAndCreateUser, getBridge } from './util/philips-hue';
-import { getAppToken, searchChannels } from './util/twitch';
 
 const fastify = Fastify();
-
-fastify.register(require('fastify-static'), {
-    root: path.join(__dirname, 'src/public')
-});
 
 const start = async () => {
     try {
